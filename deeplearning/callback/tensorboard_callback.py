aa3f39919f826cc4c87e6fa39dc375e24986c9bb
Pedro José Mora Gallegos <pedrojosemoragallegos@outlook.de>
2024-11-14 18:32:04 +0100

    updated

--- deeplearning/callback/tensorboard_callback.py
+++ deeplearning/callback/tensorboard_callback.py
@@ -1,37 +1,62 @@
+from typing import Optional
 from torch.utils.tensorboard.writer import SummaryWriter
 from .callback import Callback
 
 
 class TensorBoardCallback(Callback):
-    def __init__(self, writer: SummaryWriter):
-        self._writer: SummaryWriter = writer
-        self._global_step: int = 0
-        self._val_global_step: int = 0
+    def __init__(
+        self,
+        train_writer: SummaryWriter,
+        validation_writer: Optional[SummaryWriter],
+        test_writer: Optional[SummaryWriter],
+    ):
+        self._train_writer: SummaryWriter = train_writer
+        self._validation_writer: Optional[SummaryWriter] = validation_writer
+        self._test_writer: Optional[SummaryWriter] = test_writer
+
+        self._train_step: int = 0
+        self._validation_step: int = 0
+        self._test_step: int = 0
 
     def on_train_batch_end(self, **kwargs):
-        loss = kwargs.get("loss")
-        if loss is not None:
-            self._writer.add_scalar("Train Loss", loss, self._global_step)
-            self._global_step += 1
+        loss: int = kwargs.get("loss")  # type: ignore
+
+        self._train_writer.add_scalar("Loss/train", loss, self._train_step)
+        self._train_step += 1
 
     def on_validation_batch_end(self, **kwargs):
-        loss = kwargs.get("loss")
-        if loss is not None:
-            self._writer.add_scalar("Validation Loss", loss, self._val_global_step)
-            self._val_global_step += 1
+        if self._validation_writer:
+            loss: int = kwargs.get("loss")  # type: ignore
+
+            self._validation_writer.add_scalar(
+                "Loss/validation", loss, self._validation_step
+            )
+            self._validation_step += 1
+
+    def on_test_batch_end(self, **kwargs):
+        if self._test_writer:
+            loss: int = kwargs.get("test_batch_loss")  # type: ignore
+
+            self._test_writer.add_scalar("Loss/test", loss, self._test_step)
+            self._test_step += 1
 
     def on_train_epoch_end(self, **kwargs):
-        epoch_num = kwargs.get("epoch_num")
-        train_epoch_loss = kwargs.get("train_epoch_loss")
-        validation_loss = kwargs.get("validation_loss")
+        epoch_num: int = kwargs.get("epoch_num")  # type: ignore
+        train_epoch_loss: float = kwargs.get("train_epoch_loss")  # type: ignore
+        validation_loss: float = kwargs.get("validation_loss")  # type: ignore
 
-        if train_epoch_loss is not None:
-            self._writer.add_scalar("Epoch Loss/Train", train_epoch_loss, epoch_num)
-        if validation_loss is not None:
-            self._writer.add_scalar("Epoch Loss/Validation", validation_loss, epoch_num)
+        self._train_writer.add_scalar("Epoch Loss/Train", train_epoch_loss, epoch_num)
+
+        if self._validation_writer:
+            self._validation_writer.add_scalar(
+                "Epoch Loss/Validation", validation_loss, epoch_num
+            )
 
     def on_train_end(self, **kwargs):
-        self._writer.close()
+        self._train_writer.close()
 
-    def on_train_interrupt(self, **kwargs):
-        self._writer.close()
+        if self._validation_writer:
+            self._validation_writer.close()
+
+        if self._test_writer:
+            self._test_writer.close()
